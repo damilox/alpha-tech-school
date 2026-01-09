d2bf87f6aa50901f3106e416d037b9c509a60f94
oluwaduyilemi <favourbakare24@gmail.com>
2026-01-10 00:01:54 +0100

    commit

--- src/app/(marketing)/_component/OurSchool.tsx
+++ src/app/(marketing)/_component/OurSchool.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function OurSchool() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 w-6xl m-auto gap-12 items-center">
-      <div>
+      {/* <div>
         <p className="text-gray-800 leading-relaxed text-[18px]">
           Alpha Tech School is a tech hub dedicated to training individuals in
           practical, in-demand technology skills. With a strong foundation in
@@ -14,15 +14,15 @@ export function OurSchool() {
           positions Alpha Tech School to expand access to quality tech education
           across Nigeria, both online and offline.
         </p>
-      </div>
-      <div className="relative h-64 md:h-80 rounded-2xl overflow-hidden bg-gray-100   transition-transform duration-500">
+      </div> */}
+      {/* <div className="relative h-64 md:h-80 rounded-2xl overflow-hidden bg-gray-100   transition-transform duration-500">
         <Image
           src="/images/class.jpg"
           alt="Our Class"
           fill
           className="object-cover"
         />
-      </div>
+      </div> */}
     </div>
   );
 }
